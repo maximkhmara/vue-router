6ca044d279b4332a041712804c23da5508b51d8b
Max <maxcatalina@iMac-Max.local>
2023-03-08 11:10:23 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">Category 1</router-link>
+    <router-link to="/about">Category 2</router-link>
+    <router-link to="/about">Category 3</router-link>
+    <router-link to="/about">Category 4</router-link>
+    <router-link to="/about">Category 5</router-link>
+  </nav>
+  <router-view />
+</template>
